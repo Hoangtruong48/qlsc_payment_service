d61997840cdc79354800e9f55b8a2631079e4ae7
htruong48 <darkai6789@gmail.com>
2025-12-30 09:22:35 +0700

    init payment service

---
+++ src/main/java/com/example/qlsc_payment_service/QlscPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.qlsc_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlscPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlscPaymentServiceApplication.class, args);
+	}
+
+}
